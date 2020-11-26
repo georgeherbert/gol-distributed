98510a4bab0afdc753de3bdaef2e291ea3bf6374
George Herbert <george.herbert@icloud.com>
2020-11-26 12:09:58 +0000

    Works with client and server now – step 2 about half way completed

---
+++ engine/engine.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"strconv"
+	"sync"
+)
+
+// Converts a string receives over tcp to an integer
+func netStringToInt(netString string) int {
+	integer, _ := strconv.Atoi(netString[:len(netString) - 1])
+	return integer
+}
+
+// Initialises the world, getting the values from the server
+func initialiseWorld(height int, width int, reader *bufio.Reader) [][]byte {
+	world := make([][]byte, height)
+	for y := range world {
+		world[y] = make([]byte, width)
+	}
+	for y, row := range world {
+		for x := range row {
+			msg, _ := reader.ReadString('\n')
+			cell := netStringToInt(msg)
+			world[y][x] = byte(cell)
+		}
+	}
+	return world
+}
+
+// Returns the neighbours of a cell at given coordinates
+func getNeighbours(world [][]byte, row int, column int) []byte {
+	rowAbove, rowBelow := row - 1, row + 1
+	if row == 0 {
+		rowAbove = len(world[0]) - 1
+	} else if row == len(world[0]) - 1 {
+		rowBelow = 0
+	}
+	columnLeft, columnRight := column - 1, column + 1
+	if column == 0 {
+		columnLeft = len(world[0]) - 1
+	} else if column == len(world[0]) - 1 {
+		columnRight = 0
+	}
+	neighbours := []byte{world[rowAbove][columnLeft], world[rowAbove][column], world[rowAbove][columnRight],
+		world[row][columnLeft], world[row][columnRight], world[rowBelow][columnLeft], world[rowBelow][column],
+		world[rowBelow][columnRight]}
+	return neighbours
+}
+
+// Returns the number of live neighbours from a set of neighbours
+func calcLiveNeighbours(neighbours []byte) int {
+	liveNeighbours := 0
+	for _, neighbour := range neighbours {
+		if neighbour == 255 {
+			liveNeighbours += 1
+		}
+	}
+	return liveNeighbours
+}
+
+// Returns the new value of a cell given its current value and number of live neighbours
+func calcValue(item byte, liveNeighbours int) byte {
+	calculatedValue := byte(0)
+	if item == 255 {
+		if liveNeighbours == 2 || liveNeighbours == 3 {
+			calculatedValue = byte(255)
+		}
+	} else {
+		if liveNeighbours == 3 {
+			calculatedValue = byte(255)
+		}
+	}
+	return calculatedValue
+}
+
+// Returns the next state of a world given the current state
+func calcNextState(world [][]byte) [][]byte {
+	var nextWorld [][]byte
+	for y, row := range world {
+		nextWorld = append(nextWorld, []byte{})
+		for x, element := range row {
+			neighbours := getNeighbours(world, y, x)
+			liveNeighbours := calcLiveNeighbours(neighbours)
+			value := calcValue(element, liveNeighbours)
+			nextWorld[y] = append(nextWorld[y], value)
+		}
+	}
+	return nextWorld
+}
+
+// Returns the number of alive cells in a world
+func calcNumAliveCells(world [][]byte) int {
+	total := 0
+	for _, row := range world {
+		for _, element := range row {
+			if element == 255 {
+				total += 1
+			}
+		}
+	}
+	return total
+}
+
+// Returns the world with its final values filled
+func sendWorld(world [][]byte, conn net.Conn) {
+	for _, row := range world {
+		for _, element := range row {
+			fmt.Fprintf(conn, "%d\n", element)
+		}
+	}
+}
+
+func main() {
+	portPtr := flag.String("port", ":8030", "port to listen on")
+	ln, _ := net.Listen("tcp", *portPtr)
+	for {
+		conn, _ := ln.Accept()
+
+		reader := bufio.NewReader(conn)
+		heightString, _ := reader.ReadString('\n')
+		widthString, _ := reader.ReadString('\n')
+		turnsString, _ := reader.ReadString('\n')
+		height := netStringToInt(heightString)
+		width := netStringToInt(widthString)
+		turns := netStringToInt(turnsString)
+		world := initialiseWorld(height, width, reader)
+
+		mutexTurnsWorld := &sync.Mutex{}
+		var turn int
+		//var completedTurns int
+		for turn = 0; turn < turns; turn++ {
+			mutexTurnsWorld.Lock()
+			world = calcNextState(world)
+			//completedTurns = turn + 1
+			mutexTurnsWorld.Unlock()
+		}
+
+		sendWorld(world, conn)
+	}
+}
+
+//// Distributor divides the work between workers and interacts with other goroutines.
+//func engine(world [][]byte, turns int, turnsChan chan<- int, aliveCellsChan chan<- int) ([][]byte, int) {
+//	var turn int
+//	var completedTurns int
+//	mutexTurnsWorld := &sync.Mutex{}
+//	ticker := time.NewTicker(2 * time.Second)
+//	// Ticker
+//	go func() {
+//		for {
+//			<-ticker.C
+//			mutexTurnsWorld.Lock()
+//			turnsChan <- completedTurns
+//			aliveCellsChan <- calcNumAliveCells(world)
+//			mutexTurnsWorld.Unlock()
+//		}
+//	}()
+//	for turn = 0; turn < turns; turn++ {
+//		mutexTurnsWorld.Lock()
+//		world = calcNextState(world)
+//		completedTurns = turn + 1
+//		mutexTurnsWorld.Unlock()
+//	}
+//	return world, completedTurns
+//}
+
+
+
+
+
+
+
+
--- gol/controller.go
+++ gol/controller.go
@@ -1,6 +1,10 @@
 package gol
 
 import (
+	"bufio"
+	"fmt"
+	"net"
+
 	//"fmt"
 	"strconv"
 	"uk.ac.bris.cs/gameoflife/util"
@@ -22,18 +26,10 @@ func sendFileName(fileName string, ioCommand chan<- ioCommand, ioFileName chan<-
 }
 
 // Returns the world with its initial values filled
-func initialiseWorld(height int, width int, ioInput <-chan uint8) [][]byte {
-	world := make([][]byte, height)
-	for y := range world {
-		world[y] = make([]byte, width)
+func sendWorld(height int, width int, ioInput <-chan uint8, conn net.Conn) {
+	for i := 0; i < height * width; i++ {
+		fmt.Fprintf(conn, "%d\n", <-ioInput)
 	}
-	for y, row := range world {
-		for x := range row {
-			cell := <-ioInput
-			world[y][x] = cell
-		}
-	}
-	return world
 }
 
 // Reports the number of alive cells every time it receives data
@@ -46,6 +42,27 @@ func reportAliveCells(events chan<- Event, turnsChan <-chan int, aliveCellsChan 
 			CellsCount:     aliveCells,
 		}
 	}
+}
+
+// Converts a string receives over tcp to an integer
+func netStringToInt(netString string) int {
+	integer, _ := strconv.Atoi(netString[:len(netString) - 1])
+	return integer
+}
+
+func receiveWorld(height int, width int, reader *bufio.Reader) [][]byte {
+	world := make([][]byte, height)
+	for y := range world {
+		world[y] = make([]byte, width)
+	}
+	for y, row := range world {
+		for x := range row {
+			msg, _ := reader.ReadString('\n')
+			cell := netStringToInt(msg)
+			world[y][x] = byte(cell)
+		}
+	}
+	return world
 }
 
 // Returns a slice of alive cells
@@ -82,18 +99,47 @@ func writeFile(world [][]byte, fileName string, turns int, ioCommand chan<- ioCo
 func controller(p Params, c distributorChannels) {
 	fileName := strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight)
 	sendFileName(fileName, c.ioCommand, c.ioFileName)
-	world := initialiseWorld(p.ImageHeight, p.ImageWidth, c.ioInput)
-	turnsChan, aliveCellsChan := make(chan int), make(chan int)
-	go reportAliveCells(c.events, turnsChan, aliveCellsChan)
-	completedWorld, completedTurns := engine(world, p.Turns, turnsChan, aliveCellsChan)
-	aliveCells := getAliveCells(completedWorld)
+
+	// Dials the engine
+	conn, _ := net.Dial("tcp", "127.0.0.1:8030")
+
+	// Send the image height and width to the server
+	fmt.Fprintf(conn, "%d\n", p.ImageHeight)
+	fmt.Fprintf(conn, "%d\n", p.ImageWidth)
+
+	// Send the number of turns to the server
+	fmt.Fprintf(conn, "%d\n", p.Turns)
+
+	// Send the world to the server
+	sendWorld(p.ImageHeight, p.ImageWidth, c.ioInput, conn)
+
+	reader := bufio.NewReader(conn)
+	// Receives the world back from the server once all rounds are complete
+	world := receiveWorld(p.ImageHeight, p.ImageWidth, reader)
+
+	aliveCells := getAliveCells(world)
 	c.events <- FinalTurnComplete{
-		CompletedTurns: completedTurns,
+		CompletedTurns: p.Turns,
 		Alive:          aliveCells,
 	}
-	writeFile(completedWorld, fileName, completedTurns, c.ioCommand, c.ioFileName, c.ioOutput, c.events)
 	c.ioCommand <- ioCheckIdle // Make sure that the Io has finished any output before exiting.
 	<-c.ioIdle
-	c.events <- StateChange{completedTurns, Quitting}
+	c.events <- StateChange{p.Turns, Quitting}
 	close(c.events) // Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
+
+	//world := initialiseWorld(p.ImageHeight, p.ImageWidth, c.ioInput)
+	//
+	//turnsChan, aliveCellsChan := make(chan int), make(chan int)
+	//go reportAliveCells(c.events, turnsChan, aliveCellsChan)
+	//completedWorld, completedTurns := engine(world, p.Turns, turnsChan, aliveCellsChan)
+	//aliveCells := getAliveCells(completedWorld)
+	//c.events <- FinalTurnComplete{
+	//	CompletedTurns: completedTurns,
+	//	Alive:          aliveCells,
+	//}
+	////writeFile(completedWorld, fileName, completedTurns, c.ioCommand, c.ioFileName, c.ioOutput, c.events)
+	//c.ioCommand <- ioCheckIdle // Make sure that the Io has finished any output before exiting.
+	//<-c.ioIdle
+	//c.events <- StateChange{completedTurns, Quitting}
+	//close(c.events) // Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 }
