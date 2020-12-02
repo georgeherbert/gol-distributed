c437e428b4387e0c6f52cffad1ff9ef48dc472f5
George Herbert <george.herbert@icloud.com>
2020-12-02 17:12:40 +0000

    Updated benchmark address

--- bench_test.go
+++ bench_test.go
@@ -8,14 +8,15 @@ import (
 )
 
 func BenchmarkGol (b *testing.B) {
-	for thread := 1; thread <= 16; thread++ {
+	for thread := 1; thread <= 8; thread++ {
 		b.Run(fmt.Sprint(thread), func(b *testing.B) {
 			os.Stdout = nil // Disable all program output apart from benchmark results
 			params := gol.Params{
-				Turns:       100,
+				Turns:       25,
 				Threads:     thread,
-				ImageWidth:  512,
-				ImageHeight: 512,
+				ImageWidth:  5120,
+				ImageHeight: 5120,
+				Engine:      "3.239.113.143:8030",
 			}
 			for i := 0; i < b.N; i++ {
 				events := make(chan gol.Event)
